f19184209125cc22466758346849514d26c42c92
Favour Ezechi <ezechioriakufavour@gmail.com>
2025-08-09 15:24:05 +0100

    still tweaking

--- backend/routes/cronRoutes.js
+++ backend/routes/cronRoutes.js
@@ -5,6 +5,6 @@ import { autoCheckout } from "../controllers/cronController.js";
 const router = express.Router();
 
 // No auth for cron-job.org; secure by obscurity or token in query
-router.get("/auto-checkout", autoCheckout);
+router.post("/auto-checkout", autoCheckout);
 
 export default router;
