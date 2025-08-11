982e6d932a2b493b8dc154a6e7954c1fa8b26f11
Favour Ezechi <ezechioriakufavour@gmail.com>
2025-08-11 10:44:22 +0100

    removed the additions seemed it was affecting my deployment

--- backend/server.js
+++ backend/server.js
@@ -63,14 +63,7 @@ app.use("/api/role", roleRoutes);
 app.use("/api/property", propertyRoutes);
 app.use("/api/cron", cronRoutes); // Add cron routes
 
-if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "/frontend/dist"); // or build if CRA
-  app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(frontendPath, "index.html"))
-  );
-}
 
 // Add this simple ping endpoint
 app.get("/api/ping", (req, res) => {
